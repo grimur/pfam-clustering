73c719163c3e34bb0ff96830dbb1d4a2052eb6f7
Grimur Hjorleifsson <grimurh@gmail.com>
2018-03-29 15:51:16 +0100

    Print predictions to stdout. Strain-level vs. cluster-level predictions. Todo: remove hardcoded paths (and add sth. nicer)

--- aa_family_pred.py
+++ aa_family_pred.py
@@ -7,6 +7,9 @@ from run_model import read_pfs
 from run_model import word_count_vector
 
 
+_STRAIN = False
+
+
 def predict_cluster_aa_prob(cluster, model, aa_specificities):
     # Predict the aa content of families in the model
     # cluster: List of BGC clusters as word vectors
@@ -18,33 +21,43 @@ def predict_cluster_aa_prob(cluster, model, aa_specificities):
 
     bgc_probability_matrix = model.predict_proba(cluster)
 
+    # import cPickle
+    # with open('clsprob.bin', 'wb') as f:
+    #     cPickle.dump(bgc_probability_matrix, f)
+    # with open('clsprob.bin', 'rb') as f:
+    #     bgc_probability_matrix = cPickle.load(f)
+
     for index in xrange(len(model.components)):
         # bgc_prob = model.predict_family(cluster, index)
         bgc_prob = [x[index] for x in bgc_probability_matrix]
         norm_prob = sum(bgc_prob)
-        print norm_prob
         results = []
         for aa_id, aa_prob in aa_specificities.items():
             probability = sum([x * y for x, y in zip(bgc_prob, aa_prob)]) / norm_prob
             results.append((aa_id, probability))
         model_results.append(results)
+
     return model_results
 
 
 def main():
+    # TODO: Remove the hardcoded paths and stuff.
     modelfile = '/home/grimur/data-crusemann/mixture-crusemann-mibig-justin-lcs/NRPS/allNRPS_clustering_c0.55.cluster.model'
-    aa_dir = '/home/grimur/data-crusemann/aa-pred'
+    if _STRAIN:
+        aa_dir = '/home/grimur/data-crusemann/aa-pred-strains'
+    else:
+        aa_dir = '/home/grimur/data-crusemann/aa-pred'
     bgc_dir = '/home/grimur/data-crusemann/bigscape-crusemann-mibig-justin-lcs/pfs_nrps/'
 
     # Leave BGCs with no predictied AAs out (incl. MIBiG)
     filter_zeros = True
 
     # load model, word list
-    print 'Load model'
+    # print 'Load model'
     vocabulary, model = load_model(modelfile)
 
     # load AA specificity predictions
-    print 'Load AA specificity predictions'
+    # print 'Load AA specificity predictions'
     bgc_aa_prob = {}
     aa_index = []
     for filename in os.listdir(aa_dir):
@@ -64,14 +77,18 @@ def main():
         if filter_zeros and sum(bgc_aa) == 0.0:
             continue
         else:
-            bgc_id_fields = filename.split('.')
-            bgc_id = '.'.join(bgc_id_fields[:-2])
-            bgc_aa_prob[bgc_id] = bgc_aa
+            if not _STRAIN:
+                bgc_id_fields = filename.split('.')
+                bgc_id = '.'.join(bgc_id_fields[:-2])
+                bgc_aa_prob[bgc_id] = bgc_aa
+            else:
+                bgc_id = filename.split('_')[0]
+                bgc_aa_prob[bgc_id] = bgc_aa
 
     # load BGCs
     # preprocess BGCs
     # todo: Only run on NRPS!!! (filter the PFS files in the input directory)
-    print 'Load BGCs'
+    # print 'Load BGCs'
     bgc_collection = {}
     for filename in os.listdir(bgc_dir):
         # Only gbk files, skip files with 'final' in name
@@ -85,30 +102,48 @@ def main():
         bgc_collection[bgc_id] = vector
 
     # Filter BGCs to AA predictions and vice versa
-    print 'Merge AA and BGC predictions'
+    # print 'Merge AA and BGC predictions'
     bgc_list = []
     aa_predictions = {}
-    for bgc_id, aa_prob in bgc_aa_prob.items():
-        if bgc_id not in bgc_collection:
+    if not _STRAIN:
+        bgc_collection_keys = bgc_collection.keys()
+    else:
+        bgc_collection_keys = [x.split('.')[0] for x in bgc_collection.keys()]
+    idx = 0
+    for bgc_id, aa_prob_list in bgc_aa_prob.items():
+        if bgc_id.startswith('CNS205'):
+            print bgc_id, idx
+        idx += 1
+        if bgc_id not in bgc_collection_keys:
             continue
-        bgc_vector = bgc_collection[bgc_id]
-        bgc_list.append(bgc_vector)
-        for aa_id, aa_prob in zip(aa_index, aa_prob):
-            if aa_id in aa_predictions:
-                aa_predictions[aa_id].append(aa_prob)
-            else:
-                aa_predictions[aa_id] = [aa_prob]
-
-    print aa_predictions
+        if not _STRAIN:
+            bgc_vector = bgc_collection[bgc_id]
+            bgc_list.append(bgc_vector)
+            for aa_id, aa_prob in zip(aa_index, aa_prob_list):
+                if aa_id in aa_predictions:
+                    aa_predictions[aa_id].append(aa_prob)
+                else:
+                    aa_predictions[aa_id] = [aa_prob]
+        else:
+            for _id, bgc_vector in bgc_collection.items():
+                if _id.split('.')[0] == bgc_id:
+                    bgc_list.append(bgc_vector)
+                    for aa_id, aa_prob in zip(aa_index, aa_prob_list):
+                        if aa_id in aa_predictions:
+                            aa_predictions[aa_id].append(aa_prob)
+                        else:
+                            aa_predictions[aa_id] = [aa_prob]
 
     # Go!
-    print 'Running actual predictions'
+    # print 'Running actual predictions'
     pred = predict_cluster_aa_prob(bgc_list, model, aa_predictions)
 
-    print pred
-    with open('aa_pred_res.bin', 'wb') as f:
-        import cPickle
-        cPickle.dump(pred, f)
+    # with open('aa_pred_res_strains.bin', 'wb') as f:
+    #     import cPickle
+    #     cPickle.dump(pred, f)
+    for idx, aa_prob in enumerate(pred):
+        for aa, prob in aa_prob:
+            print '%s\t%s\t%s' % (idx, aa, prob)
 
 
 if __name__ == '__main__':
